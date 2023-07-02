ba5eae89a9738c8adbac9b13a9b8ca935d512bc0
pyrossh <peter.john@sent.com>
2023-07-02 15:08:26 +0530

    fix path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: []
+  integrations: [],
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), './src'),
+    },
+  },
 });
