7c0a38651e8d824ca1e13b802b7b648ab458338a
pyrossh <peter.john@sent.com>
2023-08-10 09:58:30 +0530

    add script

--- README.md
+++ README.md
@@ -5,14 +5,21 @@ A new Flutter project.
 ## Setup
 
 ```agsl
-brew install fluttter
-brew install cocoapods
+brew install fluttter cocoapods firebase-cli
 ```
 
 ## Lint
 ```agsl
 dart fix --apply
 dart format lib
+```
+
+## Build
+```agsl
+dart pub global activate flutterfire_cli
+flutterfire configure --project=only-bible-app
+flutter pub run flutter_launcher_icons
+flutter pub run change_app_package_name:main com.new.package.name
 ```
 
 ## Run
--- scripts/generate_audio.dart
+++ scripts/generate_audio.dart
@@ -1,14 +1,47 @@
 import "dart:io";
-import 'package:only_bible_app/utils.dart';
+import 'package:flutter_azure_tts/flutter_azure_tts.dart';
+
+Future<void> convertText(Voice v, String fileName, String text) async {
+  final ttsResponse = await AzureTts.getTts(TtsParams(
+    voice: v,
+    audioFormat: AudioOutputFormat.audio48khz96kBitrateMonoMp3,
+    rate: 0.80,
+    text: text,
+  ));
+  await File(fileName).writeAsBytes(ttsResponse.audio.buffer.asUint8List(), flush: true);
+}
 
 void main() async {
   print("starting");
-  final bibleTxt = await File("./assets/bibles/KJV.txt").readAsString();
-  final books = getBibleFromText(bibleTxt);
-  for (var book in books) {
-    for (var (chapterIndex, chapter) in book.chapters.indexed) {
-      // final outputFile = File(outputFilename).openWrite();
+  AzureTts.init(subscriptionKey: Platform.environment["SUB_KEY"]!, region: "centralindia", withLogs: false);
+  final voice = Voice(
+    name: "",
+    displayName: "",
+    localName: "",
+    shortName: "kn-IN-GaganNeural",
+    gender: "Male",
+    locale: "kn-IN",
+    sampleRateHertz: AudioOutputFormat.audio24khz48kBitrateMonoMp3,
+    voiceType: "",
+    status: "",
+  );
+  final bibleTxt = await File("./assets/bibles/Kannada.txt").readAsString();
+  final lines = bibleTxt.split("\n");
+  for (final line in lines) {
+    if (line.isEmpty) {
+      continue;
     }
+    final book = line.substring(0, 2);
+    final chapter = line.substring(3, 6);
+    final verseNo = line.substring(7, 10);
+    final verseText = line.substring(11);
+    print("${book}-${chapter}-${verseNo}.mp3");
+    final outputFilename = "./scripts/audio/${book}-${chapter}-${verseNo}.mp3";
+    final outFile = File(outputFilename);
+    if (outFile.existsSync() && outFile.lengthSync() > 100) {
+      continue;
+    }
+    await convertText(voice, outputFilename, verseText);
   }
   print("finished");
 }
