e0516279165bdceda11de92a7aef697a3ea44357
pyrossh <pyrossh>
2024-05-01 22:37:51 +0530

    prerender

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -3,10 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      strict: true,
-      fallback: '404.html'
-    })
+    adapter: adapter()
   }
 };
 
