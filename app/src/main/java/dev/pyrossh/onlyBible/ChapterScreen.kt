992672f78499fdc7e2584ba2be7509ed633ce56e
Peter John <peterjohn@Peters-MacBook-Pro.local>
2024-07-18 09:56:01 +0530

    fix swipe

--- app/src/main/java/dev/pyrossh/onlyBible/ChapterScreen.kt
+++ app/src/main/java/dev/pyrossh/onlyBible/ChapterScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
@@ -43,8 +43,10 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -53,6 +55,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
@@ -72,6 +75,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
+import kotlin.math.abs
 
 @Serializable
 @Parcelize
@@ -97,6 +101,38 @@ enum class Dir : Parcelable {
         return if (this == Left) Right else Left
     }
 }
+
+suspend fun PointerInputScope.detectSwipe(
+    swipeState: MutableIntState = mutableIntStateOf(-1),
+    onSwipeLeft: () -> Unit = {},
+    onSwipeRight: () -> Unit = {},
+    onSwipeUp: () -> Unit = {},
+    onSwipeDown: () -> Unit = {},
+) = detectDragGestures(
+    onDrag = { change, dragAmount ->
+        change.consume()
+        val (x, y) = dragAmount
+        if (abs(x) > abs(y)) {
+            when {
+                x > 0 -> swipeState.intValue = 0
+                x < 0 -> swipeState.intValue = 1
+            }
+        } else {
+            when {
+                y > 0 -> swipeState.intValue = 2
+                y < 0 -> swipeState.intValue = 3
+            }
+        }
+    },
+    onDragEnd = {
+        when (swipeState.intValue) {
+            0 -> onSwipeRight()
+            1 -> onSwipeLeft()
+            2 -> onSwipeDown()
+            3 -> onSwipeUp()
+        }
+    }
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -292,9 +328,8 @@ fun ChapterScreen(
                 .padding(innerPadding)
                 .padding(horizontal = 16.dp)
                 .pointerInput(Unit) {
-                    detectHorizontalDragGestures(onDragEnd = {
-//                            println("END " + dragAmount);
-                        if (dragAmount < 0) {
+                    detectSwipe(
+                        onSwipeLeft = {
                             val pair = Verse.getForwardPair(bookIndex, chapterIndex)
                             navController.navigate(
                                 ChapterScreenProps(
@@ -302,7 +337,8 @@ fun ChapterScreen(
                                     chapterIndex = pair.second,
                                 )
                             )
-                        } else if (dragAmount > 0) {
+                        },
+                        onSwipeRight = {
                             val pair = Verse.getBackwardPair(bookIndex, chapterIndex)
                             if (navController.previousBackStackEntry != null) {
                                 val previousBook =
@@ -334,11 +370,8 @@ fun ChapterScreen(
                                     )
                                 )
                             }
-                        }
-                    }, onHorizontalDrag = { change, da ->
-                        dragAmount = da
-                        change.consume()
-                    })
+                        },
+                    )
                 }) {
             items(chapterVerses) { v ->
                 if (v.heading.isNotEmpty()) {
--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
 ## TODO
 
-* Fix swipe calculations (look at horizontal pager)
 * Improve Paging in dark mode
 * Improve splash screen in dark mode
