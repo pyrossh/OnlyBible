ce3547a5cc7106b55221a19f5350e1d41bfcbf69
pyrossh <pyros2097@gmail.com>
2024-08-03 14:51:13 +0530

    fix bsb text

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
-import fs from "node:fs";
+import fs from 'node:fs/promises';
 import { v2 } from '@google-cloud/translate';
 
 const translate = new v2.Translate({
   key: "AIzaSyAYS5LdP5_i2AxIJprVQFYzb-7Nk2iJfv8",
 });
 
-const filename = "Telugu"
+const capitalizeFirstLetter = (s) => s.charAt(0).toUpperCase() + s.slice(1)
+
+const filename = "en_bsb"
 const code = "te"
 const outputLines = [];
-const data = fs.readFileSync(`../assets/bibles/${filename}.txt`, "utf8");
+const data = await fs.readFile(`../app/src/main/assets/bibles/${filename}.txt`, "utf8");
 const lines = data.split("\n");
 try {
   for (const line of lines) {
@@ -16,22 +18,21 @@ try {
       break;
     }
     const arr = line.split("|");
-    const book = parseInt(arr[0]);
-    const chapter = parseInt(arr[1]);
-    const verseNo = parseInt(arr[2]);
-    let heading = arr[3];
-    const verseText = arr.slice(4, arr.length).join("|");
-    if (heading) {
-      const [translation] = await translate.translate(heading, code);
-      heading = translation;
-      console.log(book, chapter, heading);
-    }
+    const bookName = arr[0];
+    const book = parseInt(arr[1]);
+    const chapter = parseInt(arr[2]);
+    const verseNo = parseInt(arr[3]);
+    let heading = arr[4];
+    const verseText = capitalizeFirstLetter(arr.slice(5, arr.length).join("|"));
+//    if (heading) {
+//      const [translation] = await translate.translate(heading, code);
+//      heading = translation;
+//      console.log(book, chapter, heading);
+//    }
     outputLines.push(`${book}|${chapter}|${verseNo}|${heading}|${verseText}`);
   }
 } catch (err) {
   console.log("err", err);
 }
 
-const outputText = outputLines.join("\n")
-
-//fs.writeFileSync(`../assets/bibles/${filename}2.txt`, outputText, "utf8")
+await fs.writeFile(`./out.txt`, outputLines.join("\n") , 'utf8');
